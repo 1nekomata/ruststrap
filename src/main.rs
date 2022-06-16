f389944d209fe16f3b88fc890710bcfbdc6f4283
1nekomata <sp.pflaumer@gmail.com>
2022-06-16 13:39:56 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io::Write;
+use std::fs::{File, OpenOptions};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut floc: String = "test.rs".to_string();
+    let mut cuse: String = String::new();
+
+    if args.len() >= 2{
+        if !args[1].is_empty() {
+            floc = args[1].clone();
+        }
+
+        let mut i: usize = 2;
+        loop {
+            cuse = cuse + "use " + &args[i] + ";" + "\n";
+            i += 1;
+            if args.len() <= i {
+                break;
+            }
+        }
+        cuse += "\n";
+    }
+    
+    let sstr: String = cuse + "fn main(){\n\n}";
+    let mut fp: File = OpenOptions::new().append(false).create(true).read(true).write(true).open(&floc).expect("Failed to open File");
+    fp.write(&sstr.into_bytes()).expect("Failed to write to File");
+
+    print!("Success!\n");
+}
